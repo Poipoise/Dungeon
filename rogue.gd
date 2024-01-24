ecb97af266dc9dd9615deb7e0243f3ecc2039ada
Andrew <apalchevs0001@mymail.lausd.net>
2024-01-24 12:21:49 -0800

    worked on dialogue

--- rogue.gd
+++ rogue.gd
@@ -1,11 +1,20 @@
 extends Node3D
+var messages = [
+	"Hello",
+	"Help Me!"
+]
 
+var typing_speed = .1
+var read_time = 2
+
+var current_message = 0
+var display = ""
+var current_char = 0
 var dialogue = preload("res://dialogue.tscn")
 var talking = false
-var d
-func _ready():
-	d = dialogue.instance()
 
 func interacted():
+	print("interacted")
+	var d = dialogue.instantiate()
 	talking = true
 	add_child(d)
